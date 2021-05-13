d0a6cb670cd3725fec4a20eed9c7c541a26e4839
Hector Adolfo <hadmarcano@gmail.com>
2021-05-12 21:49:22 -0400

    [add] firebase hosting

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const Map = () => {
+const Map = ({ data }) => {
   const mapStyle = {
     height: '50vh',
     width: '100%',
   };
 
   const defaultCenter = {
-    lat: 19.4267261,
-    lng: -99.1718796,
+    lat: data.lat,
+    lng: data.lng,
+    // lat: 19.4267261,
+    // lng: -99.1718796,
   };
 
   return (
